b36623baa356176d897c2b0a4dfe3de9a87443c3
Emerson <olimou@gmail.com>
2017-01-23 16:18:07 -0200

    change default color circle to Grey 300

--- app/src/main/java/com/olimou/android/MainActivity.java
+++ app/src/main/java/com/olimou/android/MainActivity.java
@@ -3,8 +3,13 @@ package com.olimou.android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.olimou.android.status_null.StatusNull;
 
 public class MainActivity extends AppCompatActivity {
+	public static final String TAG = MainActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
 		setSupportActionBar(toolbar);
+
+		StatusNull lStatusNull = (StatusNull) findViewById(R.id.status_null);
+
+		lStatusNull.getButton().setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.d(TAG, "onClick: ");
+			}
+		});
 	}
 
 }
